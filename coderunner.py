80554f96ae62dade2e3e375078456fa7e84be55f
Brad <jeongjooho1995@gmail.com>
2020-08-05 21:19:55 +0900

    modulized

--- coderunner.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-coderunner = Flask(__name__)
-
-
-
-@coderunner.route('/')
-@coderunner.route('/index')
-def main():
-    return render_template("index.html")
