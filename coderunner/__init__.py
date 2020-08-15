bee610cc0474c534e0df3e424228dfc8430e5d5b
root <root@DESKTOP-M84O1GJ.localdomain>
2020-08-15 22:00:37 +0900

    found the error

--- coderunner/__init__.py
+++ coderunner/__init__.py
@@ -5,24 +5,24 @@ coderunner = Flask(__name__)
 
 
 #config class
-import os
-basedir = basedir = os.path.abspath(os.path.dirname(__file__))
-class Config(object):
-    #Configuration for the database
-        # if there is a database to connect, os.environ.get('DATABASE_URL')
-        # if not it will default to the sqllite3 version
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-        # to turn off the unwanted notification
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
+# import os
+# basedir = basedir = os.path.abspath(os.path.dirname(__file__))
+# class Config(object):
+#     #Configuration for the database
+#         # if there is a database to connect, os.environ.get('DATABASE_URL')
+#         # if not it will default to the sqllite3 version
+#         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+#         'sqlite:///' + os.path.join(basedir, 'app.db')
+#         # to turn off the unwanted notification
+#         SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), index=True, unique=True)
     
 
 #routes
