76fc0f7e05112ec2c7123af050cb32c293b511b7
jinks145 <jinks14544@gmail.com>
2020-08-17 22:06:43 +0900

    merge error fix

--- coderunner/__init__.py
+++ coderunner/__init__.py
@@ -15,26 +15,6 @@ coderunner = Flask(__name__)
 import subprocess
 
 #config class
-<<<<<<< HEAD
-# import os
-# basedir = basedir = os.path.abspath(os.path.dirname(__file__))
-# class Config(object):
-#     #Configuration for the database
-#         # if there is a database to connect, os.environ.get('DATABASE_URL')
-#         # if not it will default to the sqllite3 version
-#         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-#         'sqlite:///' + os.path.join(basedir, 'app.db')
-#         # to turn off the unwanted notification
-#         SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), index=True, unique=True)
-=======
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
@@ -57,7 +37,6 @@ migrate = Migrate(coderunner, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
->>>>>>> fileupload
     
 class FileContents(db.Model):
     id = db.Column(db.Integer, primary_key=True)
