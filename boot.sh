a3bd4fac1d2daa9bfaa59a5cdeb2e9523c9ec57c
jinks145 <jinks14544@gmail.com>
2020-09-10 00:30:53 +0900

    set to postgresql db

--- boot.sh
+++ boot.sh
@@ -2,5 +2,5 @@
 flask db upgrade
 # to deal with the concurrent requests, run gunicorn with 3 workers as the current server instances have a single-core,
 # making it a optimal number of threads 
-exec gunicorn gunicorn --worker-class=gevent --worker-connections=1000 --workers= 3 -b :5000 --access-logfile - --error-logfile - coderunner:coderunner
+exec gunicorn --workers=3 -b :5000 --access-logfile - --error-logfile - coderunner:coderunner
 exec ash
