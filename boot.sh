d82fb5c67ea1d9b0b98abf69adbc5e7085e99d1d
jinks145 <jinks14544@gmail.com>
2020-09-09 22:52:07 +0900

    multi-threaded

--- boot.sh
+++ boot.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - coderunner:coderunner
+# to deal with the concurrent requests, run gunicorn with 3 workers as the current server instances have a single-core,
+# making it a optimal number of threads 
+exec gunicorn gunicorn --worker-class=gevent --worker-connections=1000 --workers= 3 -b :5000 --access-logfile - --error-logfile - coderunner:coderunner
 exec ash
